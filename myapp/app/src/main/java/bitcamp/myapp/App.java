fd68c7118d7b1703d515cda7af509af1daea3081
anhr-0819 <anhr0819@gmail.com>
2023-11-29 15:45:36 +0900

    myapp_231129_1

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -3,6 +3,8 @@
  */
 package bitcamp.myapp;
 
+import java.util.Scanner;
+
 public class App {
 
   public static void main(String[] args) {
@@ -15,13 +17,51 @@ public class App {
     String menu1 = "1.과제",
         menu2 = "2.게시글",
         menu3 = "3.도움말",
-        menu4 = "4.종료";
+        menu4 = "4.종료",
 
+        input1 = "과제입니다.",
+        input2 = "게시글입니다.",
+        input3 = "도움말입니다.",
+        input4 = "종료합니다.";
+
+    setMenu(ANSI_BOLD_RED, appTitle, ANSI_CLEAR, menu1, menu2, menu3, ANSI_RED, menu4);
+
+    Scanner scan = new Scanner(System.in);
+
+    loop:
+    while (true) {
+      System.out.print(">");
+      String menuInput = scan.nextLine();
+
+      switch (menuInput) {
+        case "1":
+          System.out.println(input1);
+          break;
+        case "2":
+          System.out.println(input2);
+          break;
+        case "3":
+          System.out.println(input3);
+          break;
+        case "4":
+          System.out.println(input4);
+          break loop;
+        case "menu":
+          setMenu(ANSI_BOLD_RED, appTitle, ANSI_CLEAR, menu1, menu2, menu3, ANSI_RED, menu4);
+          break;
+        default:
+          System.out.println("메뉴 번호가 옳지 않습니다.");
+      }
+    }
+    scan.close();
+  }
+
+  private static void setMenu(String ANSI_BOLD_RED, String appTitle, String ANSI_CLEAR,
+      String menu1, String menu2, String menu3, String ANSI_RED, String menu4) {
     System.out.println(ANSI_BOLD_RED + appTitle + ANSI_CLEAR);
     System.out.println(menu1);
     System.out.println(menu2);
     System.out.println(menu3);
     System.out.println(ANSI_RED + menu4 + ANSI_CLEAR);
-
   }
 }
