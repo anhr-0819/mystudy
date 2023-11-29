92453be19ec0a0b257b364ea1732399f893ad531
anhr-0819 <anhr0819@gmail.com>
2023-11-29 16:31:57 +0900

    myapp_231129_2

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -13,18 +13,17 @@ public class App {
     String ANSI_BOLD_RED = "\033[1;31m";
     String ANSI_RED = "\033[0;31m";
 
-    String appTitle = "[과제관리시스템]";
-    String menu1 = "1.과제",
-        menu2 = "2.게시글",
-        menu3 = "3.도움말",
-        menu4 = "4.종료",
+    String[][] menuArray = {
+        {ANSI_RED + "[과제관리시스템]" + ANSI_CLEAR, ""},
+        {"1.과제", "과제입니다."},
+        {"2.게시글", "게시글입니다."},
+        {"3.도움말", "도움말입니다."},
+        {"4.종료", ANSI_BOLD_RED + "종료합니다." + ANSI_CLEAR}
+    };
 
-        input1 = "과제입니다.",
-        input2 = "게시글입니다.",
-        input3 = "도움말입니다.",
-        input4 = "종료합니다.";
-
-    setMenu(ANSI_BOLD_RED, appTitle, ANSI_CLEAR, menu1, menu2, menu3, ANSI_RED, menu4);
+    for (String[] value : menuArray) {
+      System.out.println(value[0]);
+    }
 
     Scanner scan = new Scanner(System.in);
 
@@ -35,33 +34,26 @@ public class App {
 
       switch (menuInput) {
         case "1":
-          System.out.println(input1);
+          System.out.println(menuArray[1][1]);
           break;
         case "2":
-          System.out.println(input2);
+          System.out.println(menuArray[2][1]);
           break;
         case "3":
-          System.out.println(input3);
+          System.out.println(menuArray[3][1]);
           break;
         case "4":
-          System.out.println(input4);
+          System.out.println(menuArray[4][1]);
           break loop;
         case "menu":
-          setMenu(ANSI_BOLD_RED, appTitle, ANSI_CLEAR, menu1, menu2, menu3, ANSI_RED, menu4);
+          for (String[] strings : menuArray) {
+            System.out.println(strings[0]);
+          }
           break;
         default:
           System.out.println("메뉴 번호가 옳지 않습니다.");
       }
     }
     scan.close();
-  }
-
-  private static void setMenu(String ANSI_BOLD_RED, String appTitle, String ANSI_CLEAR,
-      String menu1, String menu2, String menu3, String ANSI_RED, String menu4) {
-    System.out.println(ANSI_BOLD_RED + appTitle + ANSI_CLEAR);
-    System.out.println(menu1);
-    System.out.println(menu2);
-    System.out.println(menu3);
-    System.out.println(ANSI_RED + menu4 + ANSI_CLEAR);
   }
 }
