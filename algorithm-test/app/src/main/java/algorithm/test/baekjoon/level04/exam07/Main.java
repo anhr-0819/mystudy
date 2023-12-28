d21fc7f8c79809f5f9072af758977fabba9a3dfe
anhr-0819 <anhr0819@gmail.com>
2023-12-28 17:54:24 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level04/exam07/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level04/exam07/Main.java
@@ -8,10 +8,24 @@ public class Main {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int[] arr = new int[28];
-    for (int i = 0; i < 28; i++) {
-      arr[i] = Integer.parseInt(br.readLine());
-    }
-  }
+    int[] arr = new int[30];
+    int[] arr2 = new int[2];
 
+    for (int i = 0; i < 28; i++) {
+      int n = Integer.parseInt(br.readLine()) - 1;
+      arr[n] = n + 1;
+    }
+    br.close();
+
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == 0) {
+        if (i + 1 > arr2[0] && arr2[0] != 0) {
+          arr2[1] = i + 1;
+        } else {
+          arr2[0] = i + 1;
+        }
+      }
+    }
+    System.out.printf("%d\n%d", arr2[0], arr2[1]);
+  }
 }
---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level04/exam08/Main.java
@@ -0,0 +1,10 @@
+package algorithm.test.baekjoon.level04.exam08;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // 수 10개를 입력받은 뒤, 42로 나눈 나머지 값이 서로 다른 것이 몇 개 있는지 출력하는 프로그램
+
+  }
+
+}
