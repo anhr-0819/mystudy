dc3a1789a4bfa6db385e888036f17e01d42cba68
anhr-0819 <anhr0819@gmail.com>
2023-12-29 18:01:37 +0900

    ,

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level04/exam08/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level04/exam08/Main.java
@@ -1,5 +1,6 @@
 package algorithm.test.baekjoon.level04.exam08;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -7,17 +8,14 @@ public class Main {
   public static void main(String[] args) {
     // 수 10개를 입력받은 뒤, 42로 나눈 나머지 값이 서로 다른 것이 몇 개 있는지 출력하는 프로그램
     Scanner scan = new Scanner(System.in);
+    HashMap<Integer, Integer> hm = new HashMap<>();
     int[] arr = new int[10];
-    int count = 0;
 
-    for (int i = 0, k = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++) {
       arr[i] = scan.nextInt() % 42;
-      count = (i == 0 || k != arr[i]) ? ++count : count;
-      System.out.print("count : " + count + " ");
-      k = arr[i];
+      hm.put(arr[i], i);
     }
-    System.out.print(count);
+    System.out.print(hm.size());
     scan.close();
   }
-
 }
---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level04/exam09/Main.java
@@ -0,0 +1,24 @@
+package algorithm.test.baekjoon.level04.exam09;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int b = scan.nextInt();
+    int m = scan.nextInt();
+    int[] arr = new int[m];
+    for (int i = 1; i <= arr.length; i++) {
+
+    }
+
+    for (int i = 0; i < m; i++) {
+      int n = scan.nextInt();
+      int m2 = scan.nextInt();
+    }
+
+
+  }
+
+}
