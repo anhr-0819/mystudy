c5cb855388b0630bf1034d6f70a72b64c2309d81
anhr-0819 <anhr0819@gmail.com>
2023-12-30 09:37:42 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level04/exam09/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level04/exam09/Main.java
@@ -6,19 +6,29 @@ public class Main {
 
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    int b = scan.nextInt();
-    int m = scan.nextInt();
-    int[] arr = new int[m];
-    for (int i = 1; i <= arr.length; i++) {
-
+    StringBuilder sb = new StringBuilder();
+    int b = scan.nextInt(); // 바구니 수
+    int m = scan.nextInt(); // 반복 횟수
+    int[] arr = new int[b];
+    for (int i = 0; i < b; i++) {
+      arr[i] = i + 1; // 바구니 번호
     }
 
-    for (int i = 0; i < m; i++) {
-      int n = scan.nextInt();
-      int m2 = scan.nextInt();
+    for (int k = 0; k < m; k++) {
+      int i = scan.nextInt() - 1;
+      int j = scan.nextInt() - 1;
+
+      for (int i2 = i, j2 = j; i2 < j2; i2++, j2--) {
+        int temp = arr[i2];
+        arr[i2] = arr[j2];
+        arr[j2] = temp;
+      }
     }
-
-
+    scan.close();
+    for (int value : arr) {
+      sb.append(value).append(" ");
+    }
+    System.out.println(sb);
   }
 
 }
