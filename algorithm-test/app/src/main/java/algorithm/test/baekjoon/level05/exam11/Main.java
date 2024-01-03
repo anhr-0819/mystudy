ab2a8c2dd686701796eca8dd78ec322df20e88e9
anhr-0819 <anhr0819@gmail.com>
2024-01-03 10:12:03 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level05/exam11/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level05/exam11/Main.java
@@ -1,26 +1,18 @@
 package algorithm.test.baekjoon.level05.exam11;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
 
   public static void main(String[] args) throws IOException {
     Scanner scan = new Scanner(System.in);
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringBuilder sb = new StringBuilder();
-    String s = "";
-    int count = 0;
+    String[] s = scan.nextLine().split(".");
 
-    while (scan.hasNextLine()) {
-      s = scan.nextLine().trim();
-      scan.nextLine();
-      // if (s.isBlank()) {
-      // break;
-      // }
-      sb.append(s).append("\n");
+    for (String str : s) {
+      System.out.print(s);
     }
     scan.close();
 
