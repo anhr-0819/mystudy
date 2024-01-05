a9adf5c3743f8a4a96c15fc2f4662e03b8566950
anhr-0819 <anhr0819@gmail.com>
2024-01-05 09:33:14 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam07/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam07/Main.java
@@ -11,55 +11,38 @@ public class Main {
     int n = scan.nextInt();
     scan.nextLine();
 
-    loop1: for (int i = 0; i < n; i++) {
-      HashMap<String, Integer> map = new HashMap<>();
-      String[] arr = scan.nextLine().split("");
-      String next = "";
+    String[] arr = new String[n];
+    int count = n;
 
-      if (arr.length == 1)
-        continue;
-      loop2: for (int k = 0; k < arr.length - 1; k++) {
-
-        try {
-          next = arr[k + 1]; // 다음 문자
-        } catch (Exception e) {
-          e.printStackTrace();
-          next = arr[k];
-        }
-        System.out.println("map = " + map.entrySet());
-
-        System.out.println("arr[k] = " + arr[k]);
-        System.out.println("next = " + next);
-
-
-
-        // 현재 문자가 다음 문자와 같지 않다면
-        if (!next.equals(arr[k])) {
-          System.out.println("현재 문자가 다음 문자와 일치하지 않음 ");
-          if (map.containsKey(arr[k]) && !arr[k].equals(arr[k - 1])) { // 일치하는 키가 존재 한다면
-            n--;
-            System.out.println("키가 존재 하므로 n--. 현재 n = " + n);
-            break loop2;
-          }
-        }
-
-        if (!map.containsKey(arr[k])) { // 일치하는 키가 없다면
-          map.put(arr[k], 1);
-          System.out.println("일치하는 키가 없으므로 맵에 추가 : " + arr[k] + " 현재 n = " + n);
-        }
-        if (!map.containsKey(next)) { // 일치하는 키가 없다면
-          map.put(next, 1);
-          System.out.println("일치하는 키가 없으므로 맵에 추가 : " + next + " 현재 n = " + n);
-        }
-        System.out.println("");
-      }
-
-      System.out.println("-----------------------------n = " + n);
-      System.out.println("실행 종료 map = " + map.entrySet());
-      System.out.println("---------------------------------------------------------");
+    for (int i = 0; i < n; i++) {
+      arr[i] = scan.nextLine();
     }
     scan.close();
 
-    System.out.println(n);
+    for (String str : arr) {
+      HashMap<String, Integer> map = new HashMap<>(str.length());
+      subLoop1: for (int k = 0; k < str.length(); k++) {
+
+        if (!map.containsKey(String.valueOf(str.charAt(k)))) {
+          map.put(String.valueOf(str.charAt(k)), 1);
+        } else {
+          continue;
+        }
+
+        int first = str.indexOf(str.charAt(k));
+        int last = str.lastIndexOf(str.charAt(k));
+
+        if (first != last) {
+          String[] subStr = str.substring(first, last).split("");
+          subLoop2: for (String s : subStr) {
+            if (!s.equals(String.valueOf(str.charAt(k)))) {
+              count = count - 1;
+              break subLoop1;
+            }
+          }
+        }
+      }
+    }
+    System.out.print(count);
   }
 }
---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam08/Main.java
@@ -0,0 +1,17 @@
+package algorithm.test.baekjoon.level06.exam08;
+
+import java.util.Map;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Map<String, Double> map = Map.of("A+", 4.5, "A0", 4.0, "B+", 3.5, "B0", 3.0, "C+", 2.0, "C0",
+        1.5, "D+", 1.5, "D0", 1.0, "F", 0.0);
+
+    for (int i = 0; i < 20; i++) {
+
+    }
+
+  }
+
+}
