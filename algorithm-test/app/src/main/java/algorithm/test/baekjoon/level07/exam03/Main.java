0c66aa89e8ee000409f3c019195530002da55c14
anhr-0819 <anhr0819@gmail.com>
2024-01-08 09:38:47 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level07/exam03/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level07/exam03/Main.java
@@ -5,9 +5,23 @@ import java.util.Scanner;
 public class Main {
 
   public static void main(String[] args) {
+    String[][] arr = new String[5][15];
     Scanner scan = new Scanner(System.in);
     for (int i = 0; i < 5; i++) {
-
+      String s = scan.nextLine();
+      for (int j = 0; j < s.length(); j++) {
+        arr[i][j] = String.valueOf(s.charAt(j));
+      }
     }
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < 15; i++) {
+      for (int j = 0; j < 5; j++) {
+        if (arr[j][i] == null)
+          continue;
+        sb.append(arr[j][i]);
+      }
+    }
+    scan.close();
+    System.out.println(sb);
   }
 }
