1cbd7f525d0c26a38d1b075967fdfd8024382f9f
anhr-0819 <anhr0819@gmail.com>
2024-01-15 09:34:27 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level07/exam04/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level07/exam04/Main.java
@@ -6,31 +6,21 @@ public class Main {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     int n = scan.nextInt();
-    int[][] arr = new int[100][100];
+    int[][] arr = new int[101][101];
     int area = 0;
-
     for (int i = 0; i < n; i++) {
-      int x = scan.nextInt() - 1;
-      int y = scan.nextInt() - 1;
-      try {
-        while (x <= x + 10) {
-          while (y <= y + 10) {
-            if (arr[x][y] == 0) {
-              area++;
-            }
-            arr[x][y] += 1;
-            System.out.print(arr[x][y]);
-            System.out.printf("arr[x][y] = %d\n", arr[x][y]);
-            System.out.printf("arr[x++][y++] = %d\n", arr[x++][y++]);
-            y++;
+      int x = scan.nextInt();
+      int y = scan.nextInt();
+      for (int x2 = x; x2 < (x + 10); x2++) {
+        for (int y2 = y; y2 < (y + 10); y2++) {
+          if (arr[x2][y2] == 0) {
+            area++;
           }
-          x++;
-          System.out.println("i = " + i);
+          arr[x2][y2] += 1;
         }
-      } catch (ArrayIndexOutOfBoundsException e) {
       }
     }
+    System.out.println(area);
     scan.close();
-    System.out.print(area);
   }
 }
