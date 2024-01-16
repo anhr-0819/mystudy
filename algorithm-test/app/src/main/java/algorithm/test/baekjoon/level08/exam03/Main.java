7b0af22658f49a6d9256602309b3bf7f066ed92b
anhr-0819 <anhr0819@gmail.com>
2024-01-16 14:18:53 +0900

    DAO 프록시 객체(스텁 객체) 자동 생성하기

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level08/exam03/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level08/exam03/Main.java
@@ -9,33 +9,38 @@ public class Main {
     int t = scan.nextInt();
     StringBuilder sb = new StringBuilder();
     for (int i = 0; i < t; i++) {
-      double d = scan.nextDouble() / 100;
+      double d = scan.nextDouble() * 0.01;
+
+      System.out.printf("d = %.2f\n", d);
       if (d >= 0.25) { // Quarter, $0.25
         sb.append((int) (d / 0.25)).append(" ");
-        d = d - (0.25 * (d / 0.25));
+        d = d - 0.25 * (int) (d / 0.25);
+        System.out.printf("Quarter, $0.25 계산 -> d = %.2f\n", d);
       } else {
         sb.append(0).append(" ");
       }
 
-      System.out.print(false);
-
       if (d >= 0.10) { // Dime, $0.10
         sb.append((int) (d / 0.10)).append(" ");
-        d = d - (0.10 * (d / 0.10));
+        d = d - (0.10 * (int) (d / 0.10));
+        System.out.printf("test = %.2f\n", d - ((0.10 * (int) (d / 0.10))));
+        System.out.printf("Dime, $0.10 계산 -> d = %.2f\n", d);
       } else {
         sb.append(0).append(" ");
       }
 
       if (d >= 0.05) { // Nickel, $0.05
         sb.append((int) (d / 0.05)).append(" ");
-        d = d - (0.05 * (d / 0.05));
+        d = d - 0.05 * (int) (d / 0.05);
+        System.out.printf("Nickel, $0.05 계산 -> d = %.2f\n", d);
       } else {
         sb.append(0).append(" ");
       }
 
       if (d >= 0.01) { // Penny, $0.01
-        sb.append((int) (d / 0.01)).append(" ");
-        d = d - (0.01 * (d / 0.01));
+        sb.append((int) (d * 100)).append(" ");
+        d = (int) (d * 100);
+        System.out.printf("Penny, $0.01 계산 -> d = %.2f\n", d);
       } else {
         sb.append(0).append(" ");
       }
