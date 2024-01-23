ca706fd8644a094ef3f9c9dc047e53f68a616831
anhr-0819 <anhr0819@gmail.com>
2024-01-23 14:42:10 +0900

    .

---
+++ algorithm-test/app/src/main/java/test/game369.java
@@ -0,0 +1,14 @@
+package test;
+
+public class game369 {
+
+  public static void main(String[] args) {
+    for (int i = 1; i <= 13; i++) {
+      String[] arr = String.valueOf(i).split("");
+      String.valueOf(i).contains("3");
+      s.contains("3");
+    }
+
+  }
+
+}
