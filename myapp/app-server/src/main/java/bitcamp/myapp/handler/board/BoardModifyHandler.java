70aefc4f97a6677eac464b0843144fe6998081b9
anhr-0819 <anhr0819@gmail.com>
2024-02-15 10:14:03 +0900

    게시글 수정 코드 중간 저장

--- myapp/app-server/src/main/java/bitcamp/myapp/handler/board/BoardModifyHandler.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/board/BoardModifyHandler.java
@@ -6,15 +6,19 @@ import bitcamp.myapp.dao.BoardDao;
 import bitcamp.myapp.vo.AttachedFile;
 import bitcamp.myapp.vo.Board;
 import bitcamp.util.Prompt;
+import bitcamp.util.TransactionManager;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BoardModifyHandler extends AbstractMenuHandler {
 
+  private TransactionManager txManager;
   private BoardDao boardDao;
   private AttachedFileDao attachedFileDao;
 
-  public BoardModifyHandler(BoardDao boardDao, AttachedFileDao attachedFileDao) {
+  public BoardModifyHandler(TransactionManager txManager, BoardDao boardDao,
+      AttachedFileDao attachedFileDao) {
+    this.txManager = txManager;
     this.boardDao = boardDao;
     this.attachedFileDao = attachedFileDao;
   }
@@ -37,41 +41,39 @@ public class BoardModifyHandler extends AbstractMenuHandler {
       board.setContent(prompt.input("내용(%s)? ", oldBoard.getContent()));
       board.setWriter(prompt.input("작성자(%s)? ", oldBoard.getWriter()));
       board.setCreatedDate(oldBoard.getCreatedDate());
-      prompt.println("첨부파일 : ");
+      prompt.println("첨부파일: ");
       for (AttachedFile file : files) {
         prompt.println(file.getFilePath());
       }
-      int inputNum = prompt.inputInt("1.추가, 2.삭제, 0.변경없음");
-
+      int inputNum = prompt.inputInt(" [1]추가 [2]삭제 [0]변경안함 -> ");
       if (inputNum == 1) {
+        List<AttachedFile> addfiles = new ArrayList<>();
         while (true) {
-          List<AttachedFile> addfiles = new ArrayList<>();
           String filepath = prompt.input("파일?(종료: 그냥 엔터) ");
           if (filepath.length() == 0) {
             break;
           }
           addfiles.add(new AttachedFile(board.getNo()).filePath(filepath));
         }
+        txManager.startTransaction();
+        attachedFileDao.addAll(addfiles);
       } else if (inputNum == 2) {
         for (AttachedFile file : files) {
-          if (prompt.input("(%s) 삭제?(y/n)", file.getFilePath()) == "y") {
+          String del = prompt.input("(%s) 삭제?(y/n) ", file.getFilePath());
+          if (del.equals("y") || del.isEmpty()) {
             attachedFileDao.delete(file.getNo());
           }
-
         }
       }
-
       boardDao.update(board);
+      txManager.commit();
       prompt.println("게시글을 변경했습니다.");
-    } catch (NumberFormatException e) {
-      prompt.println("숫자를 입력하세요!");
-
-    } catch (IllegalArgumentException e) {
-      prompt.println("게시글 변경 오류!");
-      prompt.println("다시 시도 하세요.");
-
     } catch (Exception e) {
-      prompt.println("실행 오류!");
+      try {
+        txManager.rollback();
+      } catch (Exception e2) {
+      }
+      prompt.println("게시글 변경 오류!");
       e.printStackTrace();
     }
   }
