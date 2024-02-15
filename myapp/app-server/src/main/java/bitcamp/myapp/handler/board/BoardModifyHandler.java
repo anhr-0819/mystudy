2ea27e682c18e1718ef819ffd9137504a9a89279
anhr-0819 <anhr0819@gmail.com>
2024-02-15 10:24:40 +0900

    .

--- myapp/app-server/src/main/java/bitcamp/myapp/handler/board/BoardModifyHandler.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/board/BoardModifyHandler.java
@@ -3,22 +3,15 @@ package bitcamp.myapp.handler.board;
 import bitcamp.menu.AbstractMenuHandler;
 import bitcamp.myapp.dao.AttachedFileDao;
 import bitcamp.myapp.dao.BoardDao;
-import bitcamp.myapp.vo.AttachedFile;
 import bitcamp.myapp.vo.Board;
 import bitcamp.util.Prompt;
-import bitcamp.util.TransactionManager;
-import java.util.ArrayList;
-import java.util.List;
 
 public class BoardModifyHandler extends AbstractMenuHandler {
 
-  private TransactionManager txManager;
   private BoardDao boardDao;
   private AttachedFileDao attachedFileDao;
 
-  public BoardModifyHandler(TransactionManager txManager, BoardDao boardDao,
-      AttachedFileDao attachedFileDao) {
-    this.txManager = txManager;
+  public BoardModifyHandler(BoardDao boardDao, AttachedFileDao attachedFileDao) {
     this.boardDao = boardDao;
     this.attachedFileDao = attachedFileDao;
   }
@@ -35,46 +28,17 @@ public class BoardModifyHandler extends AbstractMenuHandler {
       }
 
       Board board = new Board();
-      List<AttachedFile> files = attachedFileDao.findAllByBoardNo(no);
       board.setNo(oldBoard.getNo()); // 기존 게시글의 번호를 그대로 설정한다.
       board.setTitle(prompt.input("제목(%s)? ", oldBoard.getTitle()));
       board.setContent(prompt.input("내용(%s)? ", oldBoard.getContent()));
       board.setWriter(prompt.input("작성자(%s)? ", oldBoard.getWriter()));
       board.setCreatedDate(oldBoard.getCreatedDate());
-      prompt.println("첨부파일: ");
-      for (AttachedFile file : files) {
-        prompt.println(file.getFilePath());
-      }
-      int inputNum = prompt.inputInt(" [1]추가 [2]삭제 [0]변경안함 -> ");
-      if (inputNum == 1) {
-        List<AttachedFile> addfiles = new ArrayList<>();
-        while (true) {
-          String filepath = prompt.input("파일?(종료: 그냥 엔터) ");
-          if (filepath.length() == 0) {
-            break;
-          }
-          addfiles.add(new AttachedFile(board.getNo()).filePath(filepath));
-        }
-        txManager.startTransaction();
-        attachedFileDao.addAll(addfiles);
-      } else if (inputNum == 2) {
-        for (AttachedFile file : files) {
-          String del = prompt.input("(%s) 삭제?(y/n) ", file.getFilePath());
-          if (del.equals("y") || del.isEmpty()) {
-            attachedFileDao.delete(file.getNo());
-          }
-        }
-      }
+
       boardDao.update(board);
-      txManager.commit();
       prompt.println("게시글을 변경했습니다.");
+
     } catch (Exception e) {
-      try {
-        txManager.rollback();
-      } catch (Exception e2) {
-      }
-      prompt.println("게시글 변경 오류!");
-      e.printStackTrace();
+      prompt.println("변경 오류!");
     }
   }
 }
--- myapp/app-server/src/main/java/bitcamp/myapp/vo/AttachedFile.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/vo/AttachedFile.java
@@ -41,4 +41,9 @@ public class AttachedFile {
     this.filePath = filePath;
     return this;
   }
+
+  public AttachedFile boardNo(int boardNo) {
+    this.boardNo = boardNo;
+    return this;
+  }
 }
