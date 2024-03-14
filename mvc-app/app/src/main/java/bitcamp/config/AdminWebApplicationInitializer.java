21682584fc11c0e8d2b6689de95d3dccdd9bbb76
anhr-0819 <anhr0819@gmail.com>
2024-03-14 16:27:22 +0900

    [200~public class AdminWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {}

--- mvc-app/app/src/main/java/bitcamp/config/AdminWebApplicationInitializer.java
+++ mvc-app/app/src/main/java/bitcamp/config/AdminWebApplicationInitializer.java
@@ -8,28 +8,23 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
 
-public class AdminWebApplicationInitializer extends AbstractDispatcherServletInitializer {
+public class AdminWebApplicationInitializer extends
+    AbstractAnnotationConfigDispatcherServletInitializer {
 
   private static Log log = LogFactory.getLog(AdminWebApplicationInitializer.class);
-  AnnotationConfigWebApplicationContext rootContext;
+
 
   @Override
-  protected WebApplicationContext createRootApplicationContext() {
-    rootContext = new AnnotationConfigWebApplicationContext();
-    rootContext.register(RootConfig.class);
-    rootContext.refresh();
-    return rootContext; // ContextLoaderListener가 사용할 IoC컨테이너를 리턴
+  protected Class<?>[] getRootConfigClasses() {
+    return new Class[] {RootConfig.class};
   }
 
   @Override
-  protected WebApplicationContext createServletApplicationContext() {
-    AnnotationConfigWebApplicationContext adminContext = new AnnotationConfigWebApplicationContext();
-    adminContext.setParent(rootContext);
-    adminContext.register(AdminConfig.class);
-    adminContext.refresh();
-    return adminContext; // DispatcherServlet이 사용할 IoC컨테이너를 리턴
+  protected Class<?>[] getServletConfigClasses() {
+    return new Class[] {AdminConfig.class};
   }
 
   @Override
@@ -41,5 +36,4 @@ public class AdminWebApplicationInitializer extends AbstractDispatcherServletIni
   protected String getServletName() {
     return "admin";
   }
-
 }
--- mvc-app/app/src/main/java/bitcamp/config/AppWebApplicationInitializer.java
+++ mvc-app/app/src/main/java/bitcamp/config/AppWebApplicationInitializer.java
@@ -8,27 +8,23 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
 
-public class AppWebApplicationInitializer extends AbstractDispatcherServletInitializer {
+public class AppWebApplicationInitializer extends
+    AbstractAnnotationConfigDispatcherServletInitializer {
 
   private static Log log = LogFactory.getLog(AppWebApplicationInitializer.class);
-  AnnotationConfigWebApplicationContext rootContext;
+
 
   @Override
-  protected WebApplicationContext createRootApplicationContext() {
+  protected Class<?>[] getRootConfigClasses() {
     return null;
-    // ContextLoaderListener가 사용할 IoC컨테이너를 리턴
-    // AdminWebApplicationInitializer에서 이미 만들었기 때문에 null을 리턴한다.
-    // null이면 ContextLoaderListener를 만들지 않는다.
   }
 
   @Override
-  protected WebApplicationContext createServletApplicationContext() {
-    AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
-    appContext.register(AppConfig.class);
-    appContext.refresh();
-    return appContext; // DispatcherServlet이 사용할 IoC컨테이너를 리턴
+  protected Class<?>[] getServletConfigClasses() {
+    return new Class[] {AppConfig.class};
   }
 
   @Override
