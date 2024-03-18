6bcacdbcba53b72c96f1828c397a1e12dd0eb1d0
anhr-0819 <anhr0819@gmail.com>
2024-03-18 09:14:59 +0900

    .

--- mvc-app/app/src/main/java/bitcamp/config/App1Config.java
+++ mvc-app/app/src/main/java/bitcamp/config/App1Config.java
@@ -1,10 +1,18 @@
 package bitcamp.config;
 
+import javax.servlet.annotation.MultipartConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 
 @ComponentScan(value = "bitcamp.app1")
 public class App1Config {
-
+  @Bean
+  MultipartResolver multipartResolver() {
+    StandardServletMultipartResolver mr = new StandardServletMultipartResolver();
+    return mr; // 멀티파트로 넘어오는 파라미터를 처리해주는 객체
+  }
 }
